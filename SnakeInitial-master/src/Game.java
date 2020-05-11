de9f0618d121a4040f0ba4349098bf41bdc7cdf5
alexmontilla <56649957+alexmontilla@users.noreply.github.com>
2020-05-11 19:58:17 +0200

    node class

---
+++ SnakeInitial-master/src/Game.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xdeal
+ */
+public class Game extends javax.swing.JFrame implements NewRepainter{
+    
+     public Game() {
+    }
+     
+     public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Game().setVisible(true);
+            }
+        });
+    }
+    
+    public void repaint() {
+        super.repaint();
+    }
+}
